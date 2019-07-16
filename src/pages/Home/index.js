0bef2ebf7970ee7887b902eee8868db1ca0b5e4f
梁发鹏 <liangfapeng@liangfapengdeMacBook-Pro.local>
2019-07-16 16:54:32 +0800

    提交

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
-import { Carousel } from 'antd-mobile';
+import { Carousel, Modal, Button, Toast } from 'antd-mobile';
 import classnames from 'classnames';
 import Swiper from 'swiper/dist/js/swiper.js';
 import styles from './index.less';
@@ -31,6 +31,8 @@ class Home extends Component {
         require('../../assets/banner2.jpg'),
       ],
       imgHeight: 176,
+      showNote: false,
+      value: ''
     };
   }
 
@@ -44,7 +46,38 @@ class Home extends Component {
 
   }
 
+  // 关闭弹窗方法
+  onClose () {
+    this.setState({
+      showNote: false
+    })
+  }
+
+  // 点击投票开启弹窗
+  ClickBtn = () => {
+    this.setState({
+      showNote: true
+    })
+  }
+
+  // 对话框输入内容事件
+  handleChange (event) {
+    if (event.target.value.length > 500) {
+      Toast.info('请不要超过500字');
+    }
+    this.setState({
+      value: event.target.value
+    })
+  }
+  //编辑保存笔记弹层
+  saveNote = () => {
+    this.setState({
+      showNote: false
+    })
+  }
+
   render() {
+    const { showNote } = this.state;
     return (
       <div className={styles.wrapBox}>
 
@@ -52,8 +85,6 @@ class Home extends Component {
         <Carousel
           autoplay={false}
           infinite
-          // beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-          // afterChange={index => console.log('slide to', index)}
         >
           {this.state.bannerList.map((item, index) => (
             <a
@@ -73,15 +104,75 @@ class Home extends Component {
             </a>
           ))}
         </Carousel>
-
-        {/* <GlobalTabBar current={0} /> */}
         {/* 评选你最喜爱的一位教师 */}
         <div className={styles.contentbox}>
             <h2>评选你最喜爱的一位教师</h2>
-            <select name="" id="">
-              <option value="11">11</option>
-            </select>
+            {/* <select >
+              <option value="chrome">chrome</option>
+              <option value="safari">safari</option>
+              <option value="Edge">Edge</option>
+              <option value="firefox">firefox</option>
+              <option value="ie8">ie8</option>
+            </select> */}
+            <div className={styles.imgBox}>
+              <img src={box2Swiper1} alt="图片"/>
+              <div>校长推荐语:</div>
+              <br/>
+              <div>最受学员喜爱的师!</div>
+              <div>张兰老师</div>
+              <div className={styles.buttonBox} onClick={this.ClickBtn}>
+                投票
+              </div>
+            </div>
+            <div className={styles.imgBox}>
+              <img src={box2Swiper1} alt="图片"/>
+              <div>校长推荐语:</div>
+              <br/>
+              <div>最受学员喜爱的师!</div>
+              <div>张兰老师</div>
+              <div className={styles.buttonBox}>
+                投票
+              </div>
+            </div>
+            <div className={styles.imgBox}>
+              <img src={box2Swiper1} alt="图片"/>
+              <div>校长推荐语:</div>
+              <br/>
+              <div>最受学员喜爱的师!</div>
+              <div>张兰老师</div>
+              <div className={styles.buttonBox}>
+                投票
+              </div>
+            </div>
         </div>
+        <Modal
+          visible={showNote}
+          onClose={this.onClose.bind(this)}
+          className={styles.note_modal}
+          animationType="slide-up"
+          transparent
+          maskClosable={true}
+        >
+          <div>
+            <span>
+              亲爱的学员：
+            </span>
+            <span>
+            请写下支持老师的理由或关于老师的暖心故事，不然你最爱的老师将无法得到你宝贵的一票。（20-500字）
+            </span>
+            <textarea
+              value={this.state.value}
+              onChange={this.handleChange.bind(this)}
+              maxLength={500}
+              className={styles.note_textarea}
+              placeholder="请输入内容..."
+            />
+            <span className={styles.note_number}>{this.state.value.length}/500</span>
+            <Button onClick={() => this.saveNote()} className="save">
+              确定
+            </Button>
+          </div>
+        </Modal>
       </div>
     );
   }
